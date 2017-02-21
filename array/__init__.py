8d03b2d7f4a993e4b75908fbb828d3d37ae0168a
sapiezynski <sapiezynski@gmail.com>
2017-02-21 14:15:04 +0100

    added dict support to groupby

--- array/__init__.py
+++ array/__init__.py
@@ -9,12 +9,15 @@ def unique_rows(array):
 	_, idx = np.unique(b, return_index=True)
 	return array[idx]
 
-def groupby(array, column, presorted=True):
+def groupby(array, column, as_dict=False, presorted=True):
 	"""
 	returns a list of arrays grouped by specified column
 	"""
 	if not presorted:
 		array = array[np.argsort(array[:,column])]
 	cp = np.concatenate(([0], np.where(np.diff(array[:,column]))[0] + 1, [array.shape[0]]))
-	result = [array[cp[idx]:cp[idx+1]] for idx in range(0, cp.shape[0]-1)]
+	if as_dict:
+		return {array[cp[idx], column]: array[cp[idx]:cp[idx+1]] for idx in range(0, cp.shape[0]-1)}
+	else:
+		return [array[cp[idx]:cp[idx+1]] for idx in range(0, cp.shape[0]-1)]
 	return result
